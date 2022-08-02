75d80672781d6b196b3723535e96ac6d4ea9c8c2
Bram Janssens <bram.janssens@infosupport.com>
2022-08-02 13:31:38 +0200

    Implemented loadContributorsProgress.

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,23 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    // Based on Request4Suspend
+
+    var allUsers = emptyList<User>() // new: keep track of intermediate results to update them in the UI
+
+    // same
+    val repos = service
+        .getOrgReposCallSuspend(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    // almost same, only now we keep track of intermediate results
+    for ((index, repo) in repos.withIndex()) {
+        val users = service.getRepoContributorsCallSuspend(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+
+        allUsers = (allUsers + users).aggregate() // update intermediate results
+        updateResults(allUsers, index == repos.lastIndex) // update them in the UI and stop when index == last
+    }
 }
